1b02f4ffcf61fe2e48e9affb4c8df9dbc1004af6
vagrant <vagrant@vagrant.vm>
2020-02-05 01:35:39 +0000

    added model, requirements, seed, seed data, server, & templates

---
+++ model.py
@@ -0,0 +1,122 @@
+"""Models and database functions for Pocket Energy project."""
+
+from flask_sqlalchemy import SQLAlchemy #imports SQLAlchemy
+
+from collections import defaultdict
+
+# This is the connection to the PostgreSQL database; we're getting
+# this through the Flask-SQLAlchemy helper library. On this, we can
+# find the `session` object, where we do most of our interactions
+# (like committing, etc.)
+
+db = SQLAlchemy()
+
+#####################################################################
+# Model definitions where classes = tables
+
+class Measure(db.Model):
+    """Energy conservation measures containing carbon_payback & cost_payback"""
+
+    __tablename__ = 'conservation_measures'
+
+    c_id = db.Column(db.Integer,
+                    autoincrement = True,
+                    primary_key = True)
+    measure_name = db.Column(db.String(64), nullable = False)
+    carbon_payback = db.Column(db.String(64), nullable = True)
+    cost_payback = db.Column(db.String(64), nullable = True)
+
+
+    def __repr__(self):
+        """respresentation of the info"""
+
+        return f"<Energy Conservation Measure = {self.measure_name}, Carbon Payback (months) = {self.carbon_payback}, Cost Payback (USD$) = {self.cost_payback}>"
+
+class SolarIncentive(db.Model):
+    """Solar incentive programs containing rebates, for homeowners & commercial"""
+
+    __tablename__ = "solar_incentives"
+
+    sol_id = db.Column(db.Integer,
+                        autoincrement = True,
+                        primary_key = True)
+    c_id = db.Column(db.Integer, db.ForeignKey('conservation_measures.c_id'))                                
+
+    solar_type = db.Column(db.String(200),
+                            nullable = False)
+
+    rebate_link = db.Column(db.String(200),
+                            nullable = True)
+    def __repr__(self):
+        """respresentation of the info"""
+
+        return f"<Solar Type = {self.solar_type}, Rebates Available = {self.solar_type}>"
+
+
+class Product(db.Model):
+    """Product reccomendations, for homeowners, commercial, and renters"""
+
+    __tablename__ = "products"
+
+    prod_id = db.Column(db.Integer,
+                        autoincrement = True,
+                        primary_key = True)
+    c_id = db.Column(db.Integer, db.ForeignKey('conservation_measures.c_id'))
+    product_type = db.Column(db.String(200),
+                            nullable = False)
+    product_brand = db.Column(db.String(200),
+                            nullable = False)
+    product_model = db.Column(db.String(200),
+                            nullable = False)
+
+    product_link = db.Column(db.String(200),
+                            nullable = True)
+    def __repr__(self):
+        """respresentation of the info"""
+
+        return f"<Product Type = {self.product_type}, Brand = {self.product_brand}, Model = {self.product_model}, Link = {self.product_link}>"
+
+class Program(db.Model):
+    """General Efficiency programs,for homeowners, commercial, and renters"""
+
+    __tablename__ = "programs"
+
+    prog_id = db.Column(db.Integer,
+                        autoincrement = True,
+                        primary_key = True)
+    c_id = db.Column(db.Integer, db.ForeignKey('conservation_measures.c_id'))
+
+    program_type = db.Column(db.String(64),
+                            nullable = False)
+
+    program_link = db.Column(db.String(64),
+                            nullable = True)
+    def __repr__(self):
+        """respresentation of the info"""
+
+        return f"<Program Type = {self.program_type}, Link = {self.program_link}>"
+
+#####################################################################
+# Helper functions
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pocket_energy'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will
+    # leave you in a state of being able to work with the database
+    # directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
+
+
+    
---
+++ seed.py
@@ -0,0 +1,145 @@
+"""this file holds information to be pushed into the data tables"""
+
+import datetime, requests, os
+from sqlalchemy import func
+
+from model import Measure, SolarIncentive, Product, Program, connect_to_db, db
+from server import app
+
+
+# from model import User, Rating, Movie, connect_to_db, db
+from server import app
+
+def load_solar_incentives():
+    print('Solar Incentives')
+    file = open("seed_data/u.solarincentives")
+
+    for row in file:
+        row = row.rstrip()
+        solar_type, rebate_link = row.split(' = ')
+
+        solar_incentive = SolarIncentive(solar_type = solar_type,
+                                        rebate_link = rebate_link)
+        db.session.add(solar_incentive)
+        db.session.commit()
+    db.session.commit()
+
+
+
+
+def load_products1():
+    """Load products from u.products to database"""
+    print('Products')
+    file = open("seed_data/u.products1")
+
+
+    for row in file:
+        
+        row = row.rstrip()
+        product_type, product_route = row.split(" = ")
+
+        url = 'https://data.energystar.gov/resource/' + product_route
+            
+        response = requests.get(url)
+        data = response.json()
+        
+
+        top_3_products = [] #appends the keys & values for three products 
+        counter = 0 #ensures only three results
+        index = 0 
+        for product in data: #loops over json string
+            
+            if data[index]['meets_most_efficient_criteria'] == 'Yes' and counter < 3: #if this is the most efficient product and there are less than three products in the list
+                top_3_products.append(product['brand_name']) #add brand to product list
+                top_3_products.append(product['model_number'])
+                top_3_products.append('https://www.google.com/search?q=' + product['brand_name'] + '+' + product['model_number'])
+                counter += 1 ##add one to the counter
+            
+                product_brand = top_3_products[0]
+                product_model = top_3_products[1]
+                product_link = top_3_products[2]
+
+                product = Product(product_type = product_type,
+                            product_brand = product_brand,
+                            product_model = product_model,
+                            product_link = product_link)
+
+                db.session.add(product)
+                db.session.commit()
+
+            index += 1
+    db.session.commit()
+
+
+
+def load_products2():
+    """Load products from u.products to database"""
+    print('Products')
+    file = open("seed_data/u.products2")
+
+
+    for row in file:
+        
+        row = row.rstrip()
+        product_type, product_route = row.split(" = ")
+
+        url = 'https://data.energystar.gov/resource/' + product_route
+            
+        response = requests.get(url)
+        data = response.json()
+        
+
+        top_3_products = [] #appends the keys & values for three products 
+        counter = 0 #ensures only three results
+        index = 0
+        for product in data: #loops over json string
+            
+            if counter < 3: #if this is the most efficient product and there are less than three products in the list
+                top_3_products.append(product['brand_name']) #add brand to product list
+                top_3_products.append(product['model_number'])
+                top_3_products.append('https://www.google.com/search?q=' + product['brand_name'] + '+' + product['model_number'])
+                counter += 1 ##add one to the counter
+
+
+                product_brand = top_3_products[0]
+                product_model = top_3_products[1]
+                product_link = top_3_products[2]
+
+                product = Product(product_type = product_type,
+                            product_brand = product_brand,
+                            product_model = product_model,
+                            product_link = product_link)
+
+                db.session.add(product)
+                db.session.commit()
+
+            index += 1
+
+    db.session.commit()
+            
+def load_programs():
+    print('Utility Green Energy Programs')
+    file = open("seed_data/u.programs")
+
+    for row in file:
+        row = row.rstrip()
+        program_type, program_link = row.split(' = ')
+
+        program = Program(program_type = program_type,
+                                        program_link = program_link)
+        db.session.add(program)
+        db.session.commit()
+    db.session.commit()
+
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+    db.create_all()
+
+    load_solar_incentives()
+    load_products1()
+    load_products2()
+    load_programs()
+    
+    
