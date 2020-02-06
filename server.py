28e360fe71d22a8882e597d042cbb5c60b196502
vagrant <vagrant@vagrant.vm>
2020-02-06 00:37:04 +0000

    added front end items

--- server.py
+++ server.py
@@ -17,10 +17,42 @@ app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
-def index():
-    """Homepage."""
+def homepage():
+    """Users select building type"""
 
     return render_template("homepage.html")
+
+@app.route('/solar-options')
+def solar_options():
+    """Users select location of building"""
+#if building_type = homeowner or building_type = commercial_property
+
+    return render_template("solar.html")
+
+@app.route('/location')
+def building_location():
+    """Users select location of building"""
+
+    return render_template("location.html")
+
+@app.route('/home-efficient-products')
+def home_efficient_products():
+    """Users select what appliances and products they are interested in purchasing"""
+    
+    return render_template("home-products.html")
+
+@app.route('/commercial-efficient-products')
+def commercial_efficient_products():
+    """Users select what appliances and products they are interested in purchasing"""
+    
+    return render_template("commercial-products.html")
+
+app.route('/results')
+def efficient_products():
+    """Presents summary and comments on cost payback/ carbon payback how to proceed with suggested products"""
+    
+    return render_template("results.html")
+
 
 
 if __name__ == "__main__":
