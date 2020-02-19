b0356f6c4dc403811ef6fea5047e73fa1ad4dfcb
vagrant <vagrant@vagrant.vm>
2020-02-19 04:58:38 +0000

    sdded more React

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -2,15 +2,13 @@ class Homepage extends React.Component {
   render(){
     return (
       <div>
-        <img src="/static/images/Pocket-Energy-Logo.png" style="width:128px;height:128px;" >
+        <img src="/static/images/Pocket-Energy-Logo.png" />
 
         <p>This application provides custom energy efficiency options, reducing carbon footprint and maximizing return on investment.</p>
-
         
         <a href="/Tracking">
           Are you interested in saving $$$$ AND saving the planet ?
         </a>
-
 
       </div>
 
@@ -18,4 +16,4 @@ class Homepage extends React.Component {
   }
 }
 
-ReactDOM.render(<Homepage />, document.getElementById('app'));
+ReactDOM.render(<Homepage />, document.getElementById('homepage'));
